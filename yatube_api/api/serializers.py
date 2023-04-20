d7198ad36b7a179e2cfe01ad77ce15d0427d64ae
ocronis <sorokidzes@yandex.ru>
2023-04-20 11:37:51 +0300

    Sprint 9. Attempt 2

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -31,7 +31,7 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
-        read_only_fields = ('post', 'created', 'author')
+        read_only_fields = ('post', 'created',)
 
 
 class FollowSerializer(serializers.ModelSerializer):
